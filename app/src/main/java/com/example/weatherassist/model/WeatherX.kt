62f09c387de1e5aa23544981e565b9384dbfcc40
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 13:51:39 +0000

    data class WeatherX representing weather conditions

--- app/src/main/java/com/example/weatherassist/model/WeatherX.kt
+++ app/src/main/java/com/example/weatherassist/model/WeatherX.kt
@@ -1,16 +1,13 @@
 package com.example.weatherassist.model
 
-// Define a data class named "WeatherX" representing weather conditions
+// Declare a data class representing weather conditions
 data class WeatherX(
     // Description of the weather condition
     val description: String,
-
     // Icon representing the weather condition
     val icon: String,
-
-    // Unique identifier for the weather condition
+    // ID of the weather condition
     val id: Int,
-
     // Main category of the weather condition
     val main: String
 )
