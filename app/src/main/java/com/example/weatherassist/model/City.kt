a6db5cea02aa070fcd32639a8d4bb5cd549e6234
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 13:27:23 +0000

    Add City data class for city information

--- app/src/main/java/com/example/weatherassist/model/City.kt
+++ app/src/main/java/com/example/weatherassist/model/City.kt
@@ -1,29 +1,21 @@
-
 package com.example.weatherassist.model
 
-// Data class representing a city
+// Declare a data class representing information about a city
 data class City(
-    // Coordinates of the city
+    // Coordinate information for the city
     val coord: Coord,
-
-    // Country of the city
+    // Country code of the city
     val country: String,
-
     // Unique identifier for the city
     val id: Int,
-
     // Name of the city
     val name: String,
-
     // Population of the city
     val population: Int,
-
-    // Time of sunrise in the city (Unix timestamp)
+    // Timestamp of sunrise in the city (Unix timestamp)
     val sunrise: Int,
-
-    // Time of sunset in the city (Unix timestamp)
+    // Timestamp of sunset in the city (Unix timestamp)
     val sunset: Int,
-
-    // Timezone offset of the city in seconds
+    // Timezone offset from UTC in seconds
     val timezone: Int
 )
