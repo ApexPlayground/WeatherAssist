d6f9d7bd986b7eaea3a907d94d1e49c3cb069d41
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 13:28:12 +0000

    Add Coordinate data class for Coordinate information

--- app/src/main/java/com/example/weatherassist/model/Coord.kt
+++ app/src/main/java/com/example/weatherassist/model/Coord.kt
@@ -1,10 +1,9 @@
 package com.example.weatherassist.model
 
-// Define a data class named "Coord" representing geographical coordinates
+// Declare a data class representing geographical coordinates
 data class Coord(
-    // Latitude of the location
+    // Latitude value of the location
     val lat: Double,
-
-    // Longitude of the location
+    // Longitude value of the location
     val lon: Double
 )
