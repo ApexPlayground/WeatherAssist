f43465afd5089b2e6de8a9678dbb4a452eb25576
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 10:10:40 +0000

    Can get user info from location permission

--- app/src/main/java/com/example/weatherassist/screens/SearchScreen.kt
+++ app/src/main/java/com/example/weatherassist/screens/SearchScreen.kt
@@ -1,6 +1,15 @@
 package com.example.weatherassist.screens
 
+
 import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.net.Uri
+import android.os.Looper
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
@@ -33,7 +42,55 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.weatherassist.MainActivity
 import com.example.weatherassist.components.WeatherAppBar
+import android.provider.Settings
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import java.io.IOException
+import java.util.Locale
 
+
+// Function to request location updates
+fun requestLocationUpdates(
+    context: Context,
+    onLocationResult: (Location) -> Unit,
+    onPermissionDenied: () -> Unit
+) {
+    // Check if the location permission is granted
+    if (ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    ) {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+        val locationRequest = LocationRequest.create().apply {
+            interval = 10000 // 10 seconds
+            fastestInterval = 5000 // 5 seconds
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.lastLocation?.let {
+                    // Invoke the callback with the user's current location
+                    onLocationResult(it)
+                }
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    } else {
+        // If permission is not granted, request it
+        onPermissionDenied()
+    }
+}
 
 @Composable
 fun SearchScreen(
@@ -45,12 +102,55 @@ fun SearchScreen(
         contract = ActivityResultContracts.RequestPermission(),
         onResult = { isGranted: Boolean ->
             if (isGranted) {
-                Toast.makeText(context, "Permission already granted", Toast.LENGTH_SHORT).show()
+                // Permission granted, request location updates
+                requestLocationUpdates(
+                    context = context,
+                    onLocationResult = { location ->
+                        // Handle the user's current location (in string format)
+                        val latitude = location.latitude
+                        val longitude = location.longitude
+
+                        // Use reverse geocoding to get the city
+                        val geocoder = Geocoder(context, Locale.getDefault())
+                        try {
+                            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+                            if (addresses != null) {
+                                if (addresses.isNotEmpty()) {
+                                    val country = addresses[0].countryCode
+                                    val city = addresses[0]?.locality
+                                    val locationString = "City: $city,Country Code: $country, Lat: $latitude, Long: $longitude"
+                                    Toast.makeText(context, locationString, Toast.LENGTH_SHORT).show()
+                                } else {
+                                    Toast.makeText(context, "Unable to get city information", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        } catch (e: IOException) {
+                            Toast.makeText(context, "Error while getting city information", Toast.LENGTH_SHORT).show()
+                            e.printStackTrace()
+                        }
+                    },
+                    onPermissionDenied = {
+                        // Handle the case where the user denies location permission
+                        Toast.makeText(
+                           context,
+                            "Location permission denied",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                        // Open app settings
+
+                    }
+                )
 
 
             } else {
-                // Permission is denied, handle it accordingly
-                // You might want to show a message to the user or take appropriate action
+                // Handle the case where the user denies permission
+                Toast.makeText(
+                    context,
+                    "Location permission denied",
+                    Toast.LENGTH_SHORT
+                ).show()
+
             }
         }
     )
@@ -64,7 +164,6 @@ fun SearchScreen(
         )
 
         // Button to locate the user
-
         LocateMeButton(locationPermissionLauncher)
 
         // SearchField is a custom component for the search field
@@ -164,4 +263,11 @@ fun LocateMeButton(locationPermissionLauncher: ActivityResultLauncher<String>) {
         )
         Text(text = "Locate Me")
     }
+}
+
+fun Activity.openAppSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null)
+    ).also(::startActivity)
 }
