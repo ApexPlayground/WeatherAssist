ca94831e2fb9197614ee2682d46b0326b5e48d25
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 13:53:32 +0000

    Added data class representing system-related information

--- app/src/main/java/com/example/weatherassist/model/Sys.kt
+++ app/src/main/java/com/example/weatherassist/model/Sys.kt
@@ -1,5 +1,7 @@
 package com.example.weatherassist.model
 
+// Declare a data class representing system-related information
 data class Sys(
+    // Part of the day (e.g., 'n' for night, 'd' for day)
     val pod: String
 )
