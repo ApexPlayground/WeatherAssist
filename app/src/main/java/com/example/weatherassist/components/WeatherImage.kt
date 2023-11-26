1261ccfa71dab2c6290e450da6de19e298fbed56
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 13:37:59 +0000

    Added weather Image

---
+++ app/src/main/java/com/example/weatherassist/components/WeatherImage.kt
@@ -0,0 +1,19 @@
+package com.example.weatherassist.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.rememberImagePainter
+
+@Composable
+// Composable function to display a weather image based on the provided URL
+fun WeatherImage(imageUrl: String, modifier: Modifier = Modifier) {
+
+
+    // Using the `Image` composable with the `rememberImagePainter` to load and display the image
+    Image(
+        painter = rememberImagePainter(imageUrl),
+        contentDescription = "Weather Image",
+        modifier = modifier
+    )
+}
