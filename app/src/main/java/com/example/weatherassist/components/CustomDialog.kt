f4b231e2f4e6413025d650f3c26bfed4ae245f73
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 13:30:30 +0000

    Add CustomDialog composable for user input

--- app/src/main/java/com/example/weatherassist/components/CustomDialog.kt
+++ app/src/main/java/com/example/weatherassist/components/CustomDialog.kt
@@ -1,32 +1,76 @@
 package com.example.weatherassist.components
 
-//import android.widget.Toast
-//import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.ArrowBack
-//import androidx.compose.material.icons.filled.Favorite
-//import androidx.compose.material.icons.filled.FavoriteBorder
-//import androidx.compose.material.icons.filled.Info
-//import androidx.compose.material.icons.filled.MoreVert
-//import androidx.compose.material.icons.filled.Search
-//import androidx.compose.material.icons.filled.Settings
-//import androidx.compose.material3.CenterAlignedTopAppBar
-//import androidx.compose.material3.DropdownMenu
-//import androidx.compose.material3.DropdownMenuItem
-//import androidx.compose.material3.ExperimentalMaterial3Api
-//import androidx.compose.material3.Icon
-//import androidx.compose.material3.IconButton
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.runtime.collectAsState
-//import androidx.compose.runtime.mutableStateOf
-//import androidx.compose.runtime.remember
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.platform.LocalContext
-//import androidx.hilt.navigation.compose.hiltViewModel
-//import androidx.navigation.NavController
-//import com.example.weatherassist.model.Favorite
-//import com.example.weatherassist.screens.WeatherScreens
-//import com.example.weatherassist.viewModel.FavoriteViewModel
+//import statements
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 
-class CustomDialog {
+@Composable
+fun CustomDialog(
+    setShowDialog: (Boolean) -> Unit,
+    buttonOnClick: (String) -> Unit
+) {
+    // Dialog composable for a custom dialog
+    Dialog(onDismissRequest = { setShowDialog(false) }) {
+        // Column to arrange child composables vertically
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            // Apply styling to the column
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp)) // Rounded corners
+                .background(MaterialTheme.colorScheme.surfaceVariant) // Background color
+                .padding(5.dp) // Padding around the column
+        ) {
+            // State to hold the entered city value
+            val cityState = remember {
+                mutableStateOf("")
+            }
+
+            // Spacer for vertical spacing
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Text composable to display a label
+            Text("Enter Home City")
+
+            // Spacer for vertical spacing
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // OutlinedTextField for user input with two-way data binding
+            OutlinedTextField(
+                value = cityState.value,
+                onValueChange = {
+                    cityState.value = it
+                }
+            )
+
+            // Spacer for vertical spacing
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Button to trigger an action when clicked
+            Button(
+                onClick = {
+                    // Invoke the provided buttonOnClick function with the entered city value
+                    buttonOnClick(cityState.value)
+                }
+            ) {
+                // Text inside the button
+                Text("Save")
+            }
+        }
+    }
 }
