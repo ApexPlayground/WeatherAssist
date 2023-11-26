21c03f150efb814e12f446a5b9b49691ccc04735
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-11-26 13:52:06 +0000

    Added Enum class defining various screens in the Weather Assist app.

---
+++ app/src/main/java/com/example/weatherassist/screens/WeatherScreens.kt
@@ -0,0 +1,11 @@
+package com.example.weatherassist.screens
+
+
+// Enum class defining various screens in the Weather Assist app.
+enum class WeatherScreens {
+    SplashScreen,    // Initial screen displayed when the app is launched
+    MainScreen,      // Main weather information screen
+    SearchScreen,    // Screen for searching and entering location
+    AboutScreen,     // Screen providing information about the developer
+    SettingScreen,   // Screen for configuring app settings
+}
