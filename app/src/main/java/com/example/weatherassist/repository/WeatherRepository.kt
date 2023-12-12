018fb2b072e87797db85e5eb3bef62e9a4f45846
ApexPlayground <115173705+ApexPlayground@users.noreply.github.com>
2023-12-12 18:08:21 +0000

    Added DatabaseDao into WeatherRepository

--- app/src/main/java/com/example/weatherassist/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherassist/repository/WeatherRepository.kt
@@ -2,12 +2,16 @@ package com.example.weatherassist.repository
 
 // Import necessary dependencies
 import com.example.weatherassist.data.DataOrException
+import com.example.weatherassist.data.DatabaseDao
+import com.example.weatherassist.model.Favorite
 import com.example.weatherassist.model.Weather
 import com.example.weatherassist.network.WeatherApi
 import javax.inject.Inject
 
-// Declare the WeatherRepository class, which handles data retrieval from the API
-class WeatherRepository @Inject constructor(private val api: WeatherApi) {
+
+// WeatherRepository class handles data retrieval from the API and interacts with the local database.
+class WeatherRepository @Inject constructor(private val api: WeatherApi, private val dao: DatabaseDao) {
+
 
     // Function to get weather data for a given query
     suspend fun getWeather(query: String): DataOrException<Weather, Boolean, Exception> {
@@ -18,11 +22,30 @@ class WeatherRepository @Inject constructor(private val api: WeatherApi) {
                 DataOrException(data = response, isLoading = false)
             } else {
                 // Handle the case where the response is empty (invalid city)
-                DataOrException(data = null, exception = Exception("Invalid city"), isLoading = false)
+                DataOrException(
+                    data = null,
+                    exception = Exception("Invalid city"),
+                    isLoading = false
+                )
             }
         } catch (e: Exception) {
             // Handle exceptions during network request
             DataOrException(exception = e)
         }
     }
+
+    // Function to retrieve all favorite items from the local database
+    fun getAllFavorites() = dao.getAllFavorites()
+
+    // Function to retrieve a favorite item by its city from the local database
+    suspend fun getFavoriteById(city: String) = dao.getFavoriteById(city)
+
+    // Function to insert or update a favorite item in the local database
+    suspend fun upsertFavorite(favoriteItem: Favorite) = dao.upsertFavorite(favoriteItem)
+
+    // Function to delete a specific favorite item from the local database
+    suspend fun deleteFavorite(favoriteItem: Favorite) = dao.deleteFavorite(favoriteItem)
+
+    // Function to delete all favorite items from the local database
+    suspend fun deleteAllFavorites() = dao.deleteAllFavorites()
 }
